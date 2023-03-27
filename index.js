ae64ae550855baa4e79bc25b5fb849f9074434e3
[sharoun <[madoyasharoun@gmail.com]>
2023-03-27 22:00:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const element = document.querySelector("main")
+element.remove();
+
+
+
+let newHeader = document.createElement("h1")
+ newHeader.setAttribute("id","victory");
+ newHeader.textContent = "Sharoun is the champion";
+
+
+// const newHeader = document.createElement('h1')
+// newHeader.id = 'victory'
+// newHeader.innerHTML = "YOUR-NAME is the champion"
